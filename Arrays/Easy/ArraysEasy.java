7d6bf8e82f8111813dc070a7550627b665782bf8
Dev-Rikshith <bussarikshith@gmail.com>
2024-05-29 16:32:58 -0400

    Arrays Update

--- Arrays/Easy/ArraysEasy.java
+++ Arrays/Easy/ArraysEasy.java
@@ -1,7 +1,9 @@
+import java.util.HashSet;
+
 public class ArraysEasy {
     public static void main(String args[]){
-        Integer[] arr = new Integer[]{1,2,3,4,5};
-        secondSmallest(arr);
+        Integer[] arr = new Integer[]{1,1,2,2,3,3};
+        removeDuplicatesOptimal(arr);
     }
 
     private static void largestNumber(Integer[] arr){
@@ -39,4 +41,44 @@ public class ArraysEasy {
         }
         System.out.println(prev);
     }
+
+    private static void isSorted(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+          if (arr[i] < arr[i - 1]){
+            System.out.println("false");
+            return;
+          }
+        }
+    
+        System.out.println("true");
+    }
+
+    private static void removeDuplicates(Integer[] arr){
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        int size = set.size();
+        int j = 0;
+        for (int x: set) {
+            arr[j++] = x;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    private static void removeDuplicatesOptimal(Integer[] arr){
+        // for (int i = 0; i < arr.length; i++) {
+        //     int i = 0;
+        //     for (int j = 1; j < arr.length; j++) {
+        //         if (arr[i] != arr[j]) {
+        //             i++;
+        //             arr[i] = arr[j];
+        //         }
+        //     }
+        //     return i + 1;  REVIEWWW
+    }
+
+
 }
