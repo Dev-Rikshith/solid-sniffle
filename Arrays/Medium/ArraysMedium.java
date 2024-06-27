3081198d1b99f68831e68fb98ed8dfdbc753e61f
Dev-Rikshith <bussarikshith@gmail.com>
2024-06-27 12:29:52 -0400

    Update Arrays Medium

--- Arrays/Medium/ArraysMedium.java
+++ Arrays/Medium/ArraysMedium.java
@@ -5,11 +5,8 @@ import java.util.*;
 public class ArraysMedium {
 
     public static void main(String[] args) {
-        Integer[] arr = new Integer[]{1,2,3,4,5};
-        Integer[] ans = TwoSumOptimal(arr, 6);
-        for (Integer integer : ans) {
-            System.out.println(integer);
-        }
+        Integer[] arr = new Integer[]{4,4,2,4,3,4,4,3,2,4};
+        majorityElement(arr);
     }
 
     private static void TwoSumBetter(Integer[] arr, int target){ //For other varient return elements
@@ -44,5 +41,48 @@ public class ArraysMedium {
         }
        }
        return null;
+    }
+
+    private static Integer[] sortColors(Integer[] arr){ //Also called as Sort 0's,1's,2's problem
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr, mid, low);
+                mid++;
+                low++;
+            }
+            if(arr[mid]==1){
+                mid++;
+            }
+            if(arr[mid]==2){
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+        return arr;
+    }
+
+    private static void majorityElement(Integer[] arr){
+        int element = arr[0];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(count==0){
+                count = 1;
+                element = arr[i];
+            }else if(arr[i]==element){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        System.out.println(element);
+    }
+
+    private static void swap(Integer[] arr, int a, int b){
+        Integer temp = arr[a];
+        arr[a] =arr[b];
+        arr[b] = temp;
     }
 }
