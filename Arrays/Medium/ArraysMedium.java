646321a1ccc6d6bc4f32658672fdfdf8e7b0e874
Dev-Rikshith <bussarikshith@gmail.com>
2024-07-03 11:58:47 -0400

    Updated Arrays Medium

--- Arrays/Medium/ArraysMedium.java
+++ Arrays/Medium/ArraysMedium.java
@@ -80,6 +80,50 @@ public class ArraysMedium {
         System.out.println(element);
     }
 
+    private static int maxSubArraySum(Integer[] arr) {
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            if(sum > maxi){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+
+    private static ArrayList<Integer> maxSubArraySumFollowUp(Integer[] arr){
+        Integer maxi = Integer.MIN_VALUE;
+        Integer sum = 0;
+
+        Integer start = 0;
+        Integer ansStart = -1, ansEnd = -1;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+
+            if (sum == 0) start = i; // starting index
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        list.add(ansStart);
+        list.add(ansEnd);
+        return list;
+    }
+
     private static void swap(Integer[] arr, int a, int b){
         Integer temp = arr[a];
         arr[a] =arr[b];
